12347b1b5d258a4f34f3ea9c6af92165c6ab8d92
Pierre Karpov <karpovpierre@gmail.com>
2022-04-22 01:07:50 +0800

    remove commented out code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-// import express, { Request, Response, Application } from 'express';
-
-// const app: Application = express();
-// const PORT = process.env.PORT || 8000;
-
-// app.get("/", (req: Request, res: Response): void => {
-//     res.send("Hello Typescript with Node.js!")
-// });
-
-// app.listen(PORT, (): void => {
-//     console.log(`Server Running here 👉 https://localhost:${PORT}`);
-// });
-
 import app from './app';
 import models from './db/models';
 const { sequelize } = models;
