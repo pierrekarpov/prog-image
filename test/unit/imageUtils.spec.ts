b2ba32c8fe71d1d3bd3094d73a03c67cf9e31bc5
Pierre Karpov <karpovpierre@gmail.com>
2022-04-21 17:38:51 +0800

    more tests

--- test/unit/imageUtils.spec.ts
+++ test/unit/imageUtils.spec.ts
@@ -1,16 +1,28 @@
-import { downloadImageFromString, getFileNameFromFilePath, downloadImageFromURL, findFormatId } from '../../src/utils/imageUtils'
+import { downloadImageFromString, getFileNameFromFilePath, downloadImageFromURL, findFormatId, processChildImage } from '../../src/utils/imageUtils'
 import sinon from 'sinon';
 import * as nodeBase64Image from 'node-base64-image';
 import { FormatType, FormatTypeList } from '../../src/enums/FormatType';
+import { Media } from '../../src/db/models/Media';
 import _ from 'lodash';
 const http = require('http')
 const https = require('https');
+var jimp = require('jimp');
 
 describe('imageUtils', () => {
     let sandbox: sinon.SinonSandbox = sinon.createSandbox();
     let decodeStub;
     let httpStub;
     let httpsStub;
+    let jimpStub;
+    let mediaStub;
+
+    const childMedia = {
+        id: 2,
+        mediaSetId: 1,
+        data: {},
+        formatTypeId: 1,
+        statusTypeId: 4,
+    }
 
 
     beforeEach(async () => {
@@ -18,6 +30,8 @@ describe('imageUtils', () => {
         decodeStub = sandbox.stub(nodeBase64Image, 'decode').resolves();
         httpStub = sandbox.stub(http, 'request').returns({ end: () => { } });
         httpsStub = sandbox.stub(https, 'request').returns({ end: () => { } });
+        jimpStub = sandbox.stub(jimp, 'read').resolves({ write: (_path) => { } });
+        mediaStub = sandbox.stub(Media, 'create').resolves(childMedia as any)
     })
 
     afterEach(() => {
@@ -85,6 +99,28 @@ describe('imageUtils', () => {
             downloadImageFromURL('https://example.com', '')
             expect(httpStub.calledOnce).toBe(false);
             expect(httpsStub.calledOnce).toBe(true);
+        })
+    })
+
+    describe('processChildImage', () => {
+        const mediaSetId = 1
+        const filepath = ''
+        const formatTypeId = 1
+
+        it('should return a child with same set id', async () => {
+
+            const newChild = await processChildImage(mediaSetId, filepath, formatTypeId)
+            expect(newChild.id).toBe(childMedia.id);
+            expect(newChild.mediaSetId).toBe(childMedia.mediaSetId);
+        })
+        it('should call jimp', async () => {
+            await processChildImage(mediaSetId, filepath, formatTypeId)
+            expect(jimpStub.calledOnce).toBe(true);
+        })
+
+        it('should call db', async () => {
+            await processChildImage(mediaSetId, filepath, formatTypeId)
+            expect(mediaStub.calledOnce).toBe(true);
         })
     })
 })
